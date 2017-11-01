fe6cc51fdc33de5e3ba915e015a6da30af2b29b7
melocong <398903742@qq.com>
2017-11-02 00:41:51 +0800

    加了登录 但是注解使用失败

---
+++ src/main/java/com/orderingSystem/start.java
@@ -0,0 +1,11 @@
+package com.orderingSystem;
+
+import com.orderingSystem.view.loginFrame;
+
+public class start {
+
+    public static void main(String[] args) {
+        loginFrame loginFrame =new loginFrame();
+        loginFrame.setVisible(true);
+    }
+}
---
+++ src/main/java/com/orderingSystem/view/loginFrame.java
@@ -0,0 +1,152 @@
+/*
+ * Created by JFormDesigner on Wed Nov 01 21:01:03 CST 2017
+ */
+
+package com.orderingSystem.view;
+
+
+import com.orderingSystem.service.StaffService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * @author 123
+ */
+@Controller
+public class loginFrame extends JFrame {
+
+
+    @Autowired
+    public StaffService staffService;
+
+
+    public loginFrame() {
+        initComponents();
+
+    }
+
+    private void staffBtnActionPerformed(ActionEvent e) {
+        // TODO add your code here
+        Container contentPane = getContentPane();
+        contentPane.setLayout(null);
+        contentPane.remove(pwdLabel);
+        contentPane.remove(pwdField);
+        contentPane.repaint();
+    }
+
+    private void adminBtnActionPerformed(ActionEvent e) {
+        // TODO add your code here
+        Container contentPane = getContentPane();
+        contentPane.setLayout(null);
+        pwdLabel.setText("\u5bc6      \u7801\uff1a");
+        contentPane.add(pwdLabel);
+        pwdLabel.setBounds(175, 285, 70, 35);
+        contentPane.add(pwdField);
+        pwdField.setBounds(250, 285, 130, 35);
+        contentPane.repaint();
+    }
+
+    private void loginBtnActionPerformed(ActionEvent e) {
+        // TODO add your code here
+        String loginName = nameText.getText().toString();
+        if (StringUtils.isEmpty(loginName)){
+            JOptionPane.showMessageDialog(this,"请输入用户名","提示",JOptionPane.INFORMATION_MESSAGE);
+        }else {
+            if (staffBtn.isSelected()){
+                staffService.queryByLoginName(loginName);
+
+
+            }
+        }
+
+
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        titleLabel = new JLabel();
+        nameLabel = new JLabel();
+        nameText = new JTextField();
+        staffBtn = new JRadioButton();
+        adminBtn = new JRadioButton();
+        loginBtn = new JButton();
+        pwdLabel = new JLabel();
+        pwdField = new JPasswordField();
+
+        //======== this ========
+        setAlwaysOnTop(true);
+        setResizable(false);
+        Container contentPane = getContentPane();
+        contentPane.setLayout(null);
+
+        //---- titleLabel ----
+        titleLabel.setText("xxxx\u70b9\u9910\u7cfb\u7edf");
+        titleLabel.setFont(new Font("Abadi MT Condensed Extra Bold", Font.BOLD, 52));
+        contentPane.add(titleLabel);
+        titleLabel.setBounds(130, 50, 330, 65);
+
+        //---- nameLabel ----
+        nameLabel.setText("\u7528\u6237\u7f16\u53f7\uff1a");
+        contentPane.add(nameLabel);
+        nameLabel.setBounds(175, 170, 70, 35);
+        contentPane.add(nameText);
+        nameText.setBounds(250, 170, 130, 35);
+
+        //---- staffBtn ----
+        staffBtn.setText("\u5458\u5de5");
+        staffBtn.setSelected(true);
+        staffBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                staffBtnActionPerformed(e);
+            }
+        });
+        contentPane.add(staffBtn);
+        staffBtn.setBounds(190, 225, staffBtn.getPreferredSize().width, 45);
+
+        //---- adminBtn ----
+        adminBtn.setText("\u7ba1\u7406\u5458");
+        adminBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                adminBtnActionPerformed(e);
+            }
+        });
+        contentPane.add(adminBtn);
+        adminBtn.setBounds(280, 225, adminBtn.getPreferredSize().width, 45);
+
+        //---- loginBtn ----
+        loginBtn.setText("\u767b\u5f55");
+        loginBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                loginBtnActionPerformed(e);
+            }
+        });
+        contentPane.add(loginBtn);
+        loginBtn.setBounds(215, 370, 130, 50);
+
+        contentPane.setPreferredSize(new Dimension(580, 480));
+        pack();
+        setLocationRelativeTo(getOwner());
+
+        //---- buttonGroup1 ----
+        ButtonGroup buttonGroup1 = new ButtonGroup();
+        buttonGroup1.add(staffBtn);
+        buttonGroup1.add(adminBtn);
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    private JLabel titleLabel;
+    private JLabel nameLabel;
+    private JTextField nameText;
+    private JRadioButton staffBtn;
+    private JRadioButton adminBtn;
+    private JButton loginBtn;
+    private JLabel pwdLabel;
+    private JPasswordField pwdField;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
